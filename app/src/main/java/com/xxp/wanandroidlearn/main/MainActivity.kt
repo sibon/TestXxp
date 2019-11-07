f4a806360b0c37ba1bf9e8c2dc46e354fd397cba
sibon <1013122839@qq.com>
2019-11-07 17:40:27 +0800

    first commit

---
+++ app/src/main/java/com/xxp/wanandroidlearn/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.xxp.wanandroidlearn.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.xxp.wanandroidlearn.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
